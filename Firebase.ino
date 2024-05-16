c37534a98df961b17908bc370fdd062212869550
Rafael Brandão <72472711+RafaelBrandaoBastos@users.noreply.github.com>
2024-05-16 15:38:21 -0300

    Update Firebase.ino

--- Firebase.ino
+++ Firebase.ino
@@ -1,54 +1,87 @@
-#include <WiFi.h>                                                // esp32 library
-#include <IOXhop_FirebaseESP32.h>                                             // firebase library
-#include <ArduinoJson.h>
+#include <WiFi.h>                                               
+#include <IOXhop_FirebaseESP32.h>                                            
 
-#define FIREBASE_HOST "https://teste-casa-4d9fb-default-rtdb.firebaseio.com/"                         // the project name address from firebase id
-#define FIREBASE_AUTH "AIzaSyBSY4AEXaK-DsLq3CMcKzZFxWkSMdcbMgw"                    // the secret key generated from firebase
-#define WIFI_SSID "motorafael"                                          // input your home or public wifi name
-#define WIFI_PASSWORD "1234567890"                                    //password of wifi ssid
+//WIFI - FIREBASE - SETTINGS
+  #define FIREBASE_HOST "https://teste-casa-4d9fb-default-rtdb.firebaseio.com/"                      
+  #define FIREBASE_AUTH "AIzaSyBSY4AEXaK-DsLq3CMcKzZFxWkSMdcbMgw"                   
+  #define WIFI_SSID "2GClaroAp202"                                         
+  #define WIFI_PASSWORD "vlmrap202"  
 
-String fireStatus = "";                                                     // led status received from firebase
-int led = 13;                                                               
+//FIREBASE - DATA
+  String fireLed1 = "";                                                     
+  int led1 = 26; 
+  String fireLed2 = "";   
+  int led2 = 25; 
+  String fireLed3 = "";    
+  int led3 = 33; 
+  String fireLed4 = "";    
+  int led4 = 32;                                                                
 
 void setup() {
-
   Serial.begin(115200);
-  delay(1000);
-  pinMode(13, OUTPUT);                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                      //try to connect with wifi
-  Serial.println("Connecting to ");
-  Serial.println(WIFI_SSID);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
+  delay(500);
 
-  Serial.println();
-  Serial.println("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.println("IP Address is : ");
-  Serial.println(WiFi.localIP());                                                      //print local IP address
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
-  //Firebase.setString("fir-d-led-bang-bang", "OFF");                                          //send initial string of led status
+  //OUTPUTS
+    pinMode(26, OUTPUT); 
+    pinMode(25, OUTPUT);
+    pinMode(33, OUTPUT);
+    pinMode(32, OUTPUT);    
 
+  //WIFI
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                   
+    Serial.println("Connecting to ");
+    Serial.println(WIFI_SSID);
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(500);
+    }
+    Serial.println();
+    Serial.println("Connected to ");
+    Serial.println(WIFI_SSID);
+    Serial.println("IP Address is : ");
+    Serial.println(WiFi.localIP()); 
+
+  //FIREBASE                                                  
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                   
 }
 
 void loop() {
-  
-  fireStatus = Firebase.getString("fir-d-led-bang-bang");                     // get led status input from firebase
-  
-  if (fireStatus == "ON") {                         // compare the input of led status received from firebase
-    Serial.println("Led Turned ON");                 
-    digitalWrite(led, HIGH);                                                         // make output led ON
-  }
 
-  else if (fireStatus == "OFF") {              // compare the input of led status received from firebase
-    Serial.println("Led Turned OFF");
-    digitalWrite(led, LOW);                                                         // make output led OFF
-  }
-
-  else {
-    Serial.println("Wrong Credential! Please send ON/OFF");
-  }
-
+  //CHECK FIREBASE DATA     
+    // Check Firebase data for LED 1
+    fireLed1 = Firebase.getString("led1");
+    if (fireLed1 == "ON") {                     
+      Serial.println("LED 1 Turned ON");                 
+      digitalWrite(led1, HIGH);                                                        
+    } else {             
+      Serial.println("LED 1 Turned OFF");
+      digitalWrite(led1, LOW);                                                       
+    }
+    // Check Firebase data for LED 2
+    fireLed2 = Firebase.getString("led2");
+    if (fireLed2 == "ON") {                     
+      Serial.println("LED 2 Turned ON");                 
+      digitalWrite(led2, HIGH);                                                        
+    } else {             
+      Serial.println("LED 2 Turned OFF");
+      digitalWrite(led2, LOW);                                                       
+    }
+    // Check Firebase data for LED 3
+    fireLed3 = Firebase.getString("led3");
+    if (fireLed3 == "ON") {                     
+      Serial.println("LED 3 Turned ON");                 
+      digitalWrite(led3, HIGH);                                                        
+    } else {             
+      Serial.println("LED 3 Turned OFF");
+      digitalWrite(led3, LOW);                                                       
+    }
+    // Check Firebase data for LED 4
+    fireLed4 = Firebase.getString("led4");
+    if (fireLed4 == "ON") {                     
+      Serial.println("LED 4 Turned ON");                 
+      digitalWrite(led4, HIGH);                                                        
+    } else {             
+      Serial.println("LED 4 Turned OFF");
+      digitalWrite(led4, LOW);                                                       
+    }
 }
