7b9ad75f18a1a9f1dd41459000281451a1140012
Rafael Brandão <72472711+RafaelBrandaoBastos@users.noreply.github.com>
2024-05-21 16:58:52 -0300

    Update Firebase.ino

--- Firebase.ino
+++ Firebase.ino
@@ -1,179 +1,198 @@
-#include <WiFi.h>                                               
-#include <IOXhop_FirebaseESP32.h> 
-#include <Servo.h>   
-#include <Adafruit_AHTX0.h>  
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include <ArduinoJson.h>
+#include <DHT.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-//WIFI - FIREBASE - SETTINGS
-  #define FIREBASE_HOST "https://teste-casa-4d9fb-default-rtdb.firebaseio.com/"                      
-  #define FIREBASE_AUTH "AIzaSyBSY4AEXaK-DsLq3CMcKzZFxWkSMdcbMgw"                   
-  #define WIFI_SSID "2GClaroAp202"                                         
-  #define WIFI_PASSWORD "vlmrap202" 
-  unsigned long lastReadTime = 0;
-  unsigned long readInterval = 5000;  
+// WIFI - FIREBASE - SETTINGS
+#define FIREBASE_HOST "https://teste-casa-4d9fb-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "AIzaSyBSY4AEXaK-DsLq3CMcKzZFxWkSMdcbMgw"
+#define WIFI_SSID "2GClaroAp202"
+#define WIFI_PASSWORD "vlmrap202"
 
-//SERVO
-  Servo s1;
-  String door;  
-  int pos = 0; 
-  int x;
+// SCREEN
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-//AHTX10 - HUM - TEMP
-  float temperature = 0.0;
-  float humidity = 0.0;
-  float aht_old_temp;
-  float aht_old_humidity;
+// DHT Sensor
+#define DHT_SENSOR_PIN 12
+#define DHT_SENSOR_TYPE DHT11
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
-//FIREBASE - DATA
-  String fireLed1 = "";                                                     
-  int led1 = 26; 
-  String fireLed2 = "";   
-  int led2 = 25; 
-  String fireLed3 = "";    
-  int led3 = 33; 
-  String fireLed4 = "";    
-  int led4 = 32;                                                                
+// Firebase Data
+FirebaseData firebaseData;
+FirebaseConfig config;
+FirebaseAuth auth;
 
-//AHT 10
-  Adafruit_AHTX0 aht;
-  Adafruit_Sensor *aht_humidity, *aht_temp;
-void setup() 
+// LEDs
+bool L1 = false;
+bool L2 = false;
+bool L3 = false;
+bool L4 = false;
+bool L5 = false;
+bool L6 = false;
+bool DOOR = false;
+
+void setup()
 {
-  Serial.begin(115200);
-  delay(500);
+    Serial.begin(115200);
+    delay(500);
 
-  //SERVO
-    s1.attach(13);
-    // Initially the servo must be stopped 
-    s1.write(90);
-
-  //OUTPUTS
-    pinMode(26, OUTPUT); 
+    // OUTPUTS
+    pinMode(27, OUTPUT);
+    pinMode(14, OUTPUT);
+    pinMode(26, OUTPUT);
     pinMode(25, OUTPUT);
     pinMode(33, OUTPUT);
-    pinMode(32, OUTPUT);    
+    pinMode(32, OUTPUT);
 
-  //LED'S
-    digitalWrite(led1, LOW);
-    digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
-    digitalWrite(led4, LOW);
+    // DHT Sensor
+    dht_sensor.begin();
 
-  //WIFI
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                   
+    // SETING DISPLAY
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ;
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    display.print("Hello, world!");
+    delay(100);
+    display.clearDisplay();
+    display.setCursor(0, 10);
+    display.println("PUC MINAS:");
+    delay(500);
+    display.println("ENG.COMP - JOGOS.DIGT");
+    delay(1000);
+    display.display();
+    // WIFI
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     Serial.println("Connecting to ");
+    display.println("Connecting to" + String(WIFI_SSID));
+    delay(1000);
+    display.display();
     Serial.println(WIFI_SSID);
-    while (WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(500);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        display.print(".");
+        display.display();
+        delay(500);
     }
     Serial.println();
+    display.print("Wifi - Connected");
+    display.display();
     Serial.println("Connected to ");
     Serial.println(WIFI_SSID);
     Serial.println("IP Address is : ");
-    Serial.println(WiFi.localIP()); 
-
-  //FIREBASE                                                  
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);  
-
-  // Initialize AHT sensor
-    if (!aht.begin()) {
-      Serial.println("Failed to find AHT10/AHT20 chip");
-      while (1) {
-        delay(10);
-      }
-    }  
-    Serial.println("AHT10/AHT20 Found!");
-    aht_temp = aht.getTemperatureSensor();
-    aht_humidity = aht.getHumiditySensor();  
-    door = Firebase.getString("door"); 
+    Serial.println(WiFi.localIP());
+    delay(500);
+    // FIREBASE
+    config.host = FIREBASE_HOST;
+    config.signer.tokens.legacy_token = FIREBASE_AUTH;
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
 }
 
-void loop() 
+void loop()
 {
-  //CHECK TEMP HUM DATA
-    sensors_event_t humidity;
-    sensors_event_t temp;
-    aht_humidity->getEvent(&humidity);
-    aht_temp->getEvent(&temp);
-    Serial.println("-Sensor terperature: " + String(temp.temperature));
-    Serial.println("-Sensor humidity: " + String(humidity.relative_humidity));
-    if (int(aht_old_temp) != int(temp.temperature)) {
-      aht_temp->getEvent(&temp);
-      Firebase.setString("temp", String(temp.temperature));
-      Serial.println("-Updated temperature");
-    }
-    if (int(aht_old_humidity) != int(humidity.relative_humidity)) {
-      aht_humidity->getEvent(&humidity);
-      Firebase.setString("hum", String(humidity.relative_humidity));
-      Serial.println("-Updated humidity");
-    }
-    aht_old_temp = temp.temperature;
-    aht_old_humidity = humidity.relative_humidity;
+    display.clearDisplay();
+    display.setCursor(0, 10);
+    display.setTextSize(2);
+    // Lê o JSON do Firebase
+    if (Firebase.getJSON(firebaseData, "/ATUADORES"))
+    {
+        if (firebaseData.dataType() == "json")
+        {
+            // Obter a string JSON
+            String jsonString;
+            firebaseData.jsonObject().toString(jsonString, true);
+            FirebaseJson &json = firebaseData.jsonObject();
+            // Printar a string JSON
+            Serial.println("JSON recebido:");
+            Serial.println(jsonString);
 
-    //CHECK FIREBASE    
-      // Check if it's time to read from Firebase
-      Serial.println("\n===ESP32-CICLE===\n-Firebase Request: ");
-        //CHECK DOORS
-        if (door != Firebase.getString("door")) {
-          door = Firebase.getString("door");         
-          if (door == "ON") {
-            // Start turning clockwise
-            s1.write(0);
-            // Go on turning for the right duration
-            delay(300);
-            // Stop turnings
-            s1.write(90);                                                      
-          }
-          if (door == "OFF") { 
-            // Start turning clockwise
-            s1.write(100);
-            // Go on turning for the right duration
-            delay(300);
-            // Stop turning
-            s1.write(90);                                                        
-          }
-        }
+            // Decodificar o JSON e acessar os valores das chaves como booleanos
+            StaticJsonDocument<256> doc;
+            DeserializationError error = deserializeJson(doc, jsonString);
+            if (error)
+            {
+                Serial.print("Falha ao decodificar JSON: ");
+                Serial.println(error.c_str());
+                return;
+            }
 
-        //CHECK LIGHT
-        lastReadTime = millis();
-        if (fireLed1 != Firebase.getString("led1")) {
-          fireLed1 = Firebase.getString("led1");
-          if (fireLed1 == "ON") {                     
-            Serial.print(" Led1-ON");                 
-            digitalWrite(led1, HIGH);                                                        
-          } else {             
-            Serial.print(" Led1-OFF");
-            digitalWrite(led1, LOW);                                                       
-          }
+            L1 = doc["L1"];
+            L2 = doc["L2"];
+            L3 = doc["L3"];
+            L4 = doc["L4"];
+            L5 = doc["L5"];
+            L6 = doc["L6"];
+            DOOR = doc["DOOR"];
+            Serial.println("Valores das chaves:");
+            Serial.println(" L1:" + String(L1) + " L2:" + String(L2) + " L3:" + String(L3) + " L4:" + String(L4) + " L5:" + String(L5) + " L6:" + String(L6) + " DOOR:" + DOOR);
         }
-        if (fireLed2 != Firebase.getString("led2")) {
-          fireLed2 = Firebase.getString("led2");
-          if (fireLed2 == "ON") {                     
-            Serial.print(" Led2-ON");                 
-            digitalWrite(led2, HIGH);                                                        
-          } else {             
-            Serial.print(" Led2-OFF");
-            digitalWrite(led2, LOW);                                                       
-          }
-        }
-        if (fireLed3 != Firebase.getString("led3")) {
-          fireLed3 = Firebase.getString("led3");
-          if (fireLed3 == "ON") {                     
-            Serial.print(" Led3-ON");                 
-            digitalWrite(led3, HIGH);                                                        
-          } else {             
-            Serial.print(" Led3-OFF");
-            digitalWrite(led3, LOW);                                                       
-          }
-        }
-        if (fireLed4 != Firebase.getString("led4")) {
-          fireLed4 = Firebase.getString("led4");
-          if (fireLed4 == "ON") {                     
-            Serial.println(" Led4-ON");                 
-            digitalWrite(led4, HIGH);                                                        
-          } else {             
-            Serial.println(" Led4-OFF");
-            digitalWrite(led4, LOW);                                                       
-          }
-        }  
+    }
+    else
+    {
+        Serial.print("Erro ao obter JSON: ");
+        Serial.println(firebaseData.errorReason());
+    }
+    //LED CONTROL 
+    if(L1 == true) {
+      digitalWrite(14, HIGH); 
+    } else {
+      digitalWrite(14, LOW); 
+    }
+    if(L2 == true) {
+      digitalWrite(27, HIGH); 
+    } else {
+      digitalWrite(27, LOW); 
+    }
+    if(L3 == true) {
+      digitalWrite(26, HIGH); 
+    } else {
+      digitalWrite(26, LOW); 
+    }
+    if(L4 == true) {
+      digitalWrite(25, HIGH); 
+    } else {
+      digitalWrite(25, LOW); 
+    }
+    if(L5 == true) {
+      digitalWrite(33, HIGH); 
+    } else {
+      digitalWrite(33, LOW); 
+    }
+    if(L6 == true) {
+      digitalWrite(32, HIGH); 
+    } else {
+      digitalWrite(32, LOW); 
+    }
+
+    // read humidity
+    int humi = trunc(dht_sensor.readHumidity());
+    // read temperature in Celsius
+    int tempC = trunc(dht_sensor.readTemperature());
+    // read temperature in Fahrenheit
+    int tempF = trunc(dht_sensor.readTemperature(true));
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+    Serial.print("  |  ");
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C  ~  ");
+    Serial.print(tempF);
+    Serial.println("°F");
+    display.println("TEMP " + String(tempC) + "C");
+    display.println("HUM " + String(humi) + "%");
+    display.display();
+    delay(1000);
 }
